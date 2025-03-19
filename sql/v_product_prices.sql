31799d25f4ae4bcb25c4ef2fd9d8b2b13c781517
Jorge Luis <jorgequiguango@outlook.com>
2025-03-19 13:50:06 -0500

    [FIX] View right join

--- sql/v_product_prices.sql
+++ sql/v_product_prices.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE VIEW v_product_prices AS
-    SELECT
-        p.cod_articulo    code,
-        p.codigo_barras   barcode,
-        p.nombre_articulo name,
-        price.aux_precio  price,
-        price.cod_unidad  uom
-    FROM
-        inv_articulo          p
-        LEFT JOIN fac_catalogo_precio_d price ON p.cod_articulo = price.cod_articulo;
+SELECT
+    p.cod_articulo    code,
+    p.codigo_barras   barcode,
+    p.nombre_articulo name,
+    price.aux_precio  price,
+    price.cod_unidad  uom
+FROM
+    fac_catalogo_precio_d price
+    RIGHT JOIN inv_articulo p ON p.cod_articulo = price.cod_articulo;
